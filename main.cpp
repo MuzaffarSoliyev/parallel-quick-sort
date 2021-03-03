211caf81e84f6a024ca1dcab7a4e1e00a2e2be5b
MuzaffarSoliyev <muzaffar_soliyev@mail.ru>
2021-03-03 23:56:34 +0500

    remade algorithm

--- main.cpp
+++ main.cpp
@@ -1,180 +1,248 @@
 #include <algorithm>
-#include <atomic>
-#include <chrono>
+#include <cmath>
+#include <condition_variable>
 #include <iostream>
+#include <mutex>
 #include <thread>
-#include <algorithm>
-#include <ctime>
-#include "qsort.h"
+#include <vector>
+#include <fstream>
 
+const int THREAD_COUT = 8;
+// const int ARRAY_SIZE = 1000000;
 using namespace std;
 
-//const int ARRAY_SIZE = 100000;
+vector<bool> is_sort_parts(THREAD_COUT);
+vector<bool> is_sort_parts1((int)(THREAD_COUT / 2));
+vector<bool> is_sort_parts2((int)(THREAD_COUT / 4));
+
+bool ret() {
+  for (int i = 0; i < THREAD_COUT; i++) {
+    if (is_sort_parts[i] == false) {
+      return false;
+    }
+  }
+  return true;
+}
+bool ret1() {
+  for (int i = 0; i < (int)(THREAD_COUT / 2); i++) {
+    if (is_sort_parts1[i] == false) {
+      return false;
+    }
+  }
+  return true;
+}
+bool ret2() {
+  for (int i = 0; i < (int)(THREAD_COUT / 4); i++) {
+    if (is_sort_parts2[i] == false) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void print_array(vector<int>& arr, int n) {
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
 
 void generate_arr(vector<int>& arr) {
   generate(arr.begin(), arr.end(), []() { return rand() % 100; });
 }
 
-void parallel_sort(vector<int>& arr, int n, int ARRAY_SIZE) {
+int partition(vector<int>& numbers, int left, int right) {
+  int pivot = numbers[right];
+  int i = left - 1;
 
-  //cout << "COUNT OF THREAD: " << n << endl;
-  int step = ARRAY_SIZE / n;
-
-  vector<pair<int, int>> steps;
-  for (int i = 0; i < n; i++) {
-    steps.push_back({i * step, (i + 1) * step - 1});
+  for (int j = left; j <= right - 1; j++) {
+    if (numbers[j] < pivot) {
+      i++;
+      swap(numbers[i], numbers[j]);
+    }
   }
-  steps[n - 1].second = ARRAY_SIZE - 1;
-  thread* ths = new thread[n];
+  swap(numbers[i + 1], numbers[right]);
+  i++;
+  return i;
+}
 
-  for (int i = 0; i < n; i++) {
-    ths[i] = thread(quickSort, ref(arr), steps[i].first, steps[i].second);
+void quickSort(vector<int>& numbers, int left, int right) {
+  if (left < right) {
+    int middle = partition(numbers, left, right);
+
+    quickSort(numbers, left, middle - 1);
+    quickSort(numbers, middle + 1, right);
   }
-  for (int i = 0; i < n; i++) {
-    ths[i].join();
+}
+void merge(vector<int>& arr_1, int start_1, int start_2, int end_1, int end_2,
+           int n, int m) {
+  vector<int> merged_array(n + m);
+  int j = 0, i = 0;
+
+  while (i + j < n + m) {
+    if (j == m || (i < n && arr_1[start_1 + i] < arr_1[start_2 + j])) {
+      merged_array[i + j] = arr_1[start_1 + i];
+      i++;
+    } else {
+      merged_array[i + j] = arr_1[start_2 + j];
+      j++;
+    }
   }
-  /*
-  cout << "Quicksort: " << endl;
-
-  for (auto& elem : arr) {
-    cout << elem << " ";
+  for (int i = start_1, j = 0; i <= end_2; i++, j++) {
+    arr_1[i] = merged_array[j];
   }
+}
 
-  cout << endl;*/
-  if (n == 2) {
-    ths[0] =
-        thread(merge, ref(arr), steps[0].first, steps[1].first, steps[0].second,
-               steps[1].second, steps[0].second - steps[0].first + 1,
-               steps[1].second - steps[1].first + 1, 0);
-    ths[0].join();
-  } else if (n == 4) {
-    ths[0] =
-        thread(merge, ref(arr), steps[0].first, steps[1].first, steps[0].second,
-               steps[1].second, steps[0].second - steps[0].first + 1,
-               steps[1].second - steps[1].first + 1, 0);
-    ths[1] =
-        thread(merge, ref(arr), steps[2].first, steps[3].first, steps[2].second,
-               steps[3].second, steps[2].second - steps[2].first + 1,
-               steps[3].second - steps[3].first + 1, 1);
+mutex mtx, mtx1, mtx2;
+condition_variable cv, cv1, cv2;
 
-    ths[0].join();
-    ths[1].join();
+void parallel_function(vector<int>& numbers, vector<pair<int, int>> steps,
+                       int thread_index, int n) {
+  quickSort(numbers, steps[thread_index].first, steps[thread_index].second);
+  is_sort_parts[thread_index] = true;
+  int iter = 0;
+  while (iter < log2(n)) {
+    if (thread_index % 2 == 0 && iter == 0) {
+      unique_lock<mutex> ulm(mtx);
+      cv.wait(ulm, ret);
 
-    ths[0] =
-        thread(merge, ref(arr), steps[0].first, steps[2].first, steps[1].second,
-               steps[3].second, steps[1].second - steps[0].first + 1,
-               steps[3].second - steps[2].first + 1, 0);
-    ths[0].join();
+      merge(numbers, steps[thread_index].first, steps[thread_index + 1].first,
+            steps[thread_index].second, steps[thread_index + 1].second,
+            steps[thread_index].second - steps[thread_index].first + 1,
+            steps[thread_index + 1].second - steps[thread_index + 1].first + 1);
+      is_sort_parts1[thread_index / 2] = true;
+      // print_array(numbers, ARRAY_SIZE);
+      // cout << "NNNN: " << thread_index << "---" <<
+      // is_sort_parts1[thread_index/2] << endl;
+    } else {
+      cv.notify_one();
+    }
+    if (thread_index % 4 == 0 && iter == 1) {
+      // cout << "THREAD ID: " << thread_index << endl;
+      // cout << "NNNN: " << thread_index << "---" <<
+      // is_sort_parts[thread_index] << endl;
+      unique_lock<mutex> ulm1(mtx1);
+      cv1.wait(ulm1, ret1);
+      merge(numbers, steps[thread_index].first, steps[thread_index + 2].first,
+            steps[thread_index + 1].second, steps[thread_index + 3].second,
+            steps[thread_index + 1].second - steps[thread_index].first + 1,
+            steps[thread_index + 3].second - steps[thread_index + 2].first + 1);
+      is_sort_parts2[thread_index / 4] = true;
+      // cout << "NNNN: " << thread_index << "---" <<
+      // is_sort_parts2[thread_index/4] << endl; print_array(numbers,
+      // ARRAY_SIZE);
+    } else {
+      cv1.notify_one();
+    }
+    if (thread_index % 8 == 0 && iter == 2) {
+      // cout << "THREAD ID: " << thread_index << endl;
+      // cout << "NNNN: " << thread_index << "---" <<
+      // is_sort_parts[thread_index] << endl;
+      unique_lock<mutex> ulm2(mtx2);
+      cv2.wait(ulm2, ret2);
+      merge(numbers, steps[thread_index].first,
+            steps[thread_index + 2 * 2].first,
+            steps[thread_index + 2 * 1 + 1].second,
+            steps[thread_index + 2 * 3 + 1].second,
+            steps[thread_index + 2 * 2 + 1].second - steps[thread_index].first +
+                1,
+            steps[thread_index + 2 * 3 + 1].second -
+                steps[thread_index + 2 * 2].first + 1);
+      // cout << "NNNN: " << thread_index << "---" <<
+      // is_sort_parts[thread_index] << endl; print_array(numbers, ARRAY_SIZE);
+    } else {
+      cv2.notify_one();
+    }
 
-  } else if (n == 8) {
-    ths[0] =
-        thread(merge, ref(arr), steps[0].first, steps[1].first, steps[0].second,
-               steps[1].second, steps[0].second - steps[0].first + 1,
-               steps[1].second - steps[1].first + 1, 0);
-    ths[1] =
-        thread(merge, ref(arr), steps[2].first, steps[3].first, steps[2].second,
-               steps[3].second, steps[2].second - steps[2].first + 1,
-               steps[3].second - steps[3].first + 1, 1);
-    ths[2] =
-        thread(merge, ref(arr), steps[4].first, steps[5].first, steps[4].second,
-               steps[5].second, steps[4].second - steps[4].first + 1,
-               steps[5].second - steps[5].first + 1, 2);
-    ths[3] =
-        thread(merge, ref(arr), steps[6].first, steps[7].first, steps[6].second,
-               steps[7].second, steps[6].second - steps[6].first + 1,
-               steps[7].second - steps[7].first + 1, 3);
+    iter++;
+  }
+}
 
-    ths[0].join();
-    ths[1].join();
-    ths[2].join();
-    ths[3].join();
+void show_parts() {
+  for (int i = 0; i < is_sort_parts.size(); i++) {
+    cout << is_sort_parts[i] << " ";
+  }
+  cout << endl;
+  for (int i = 0; i < is_sort_parts1.size(); i++) {
+    cout << is_sort_parts1[i] << " ";
+  }
+  cout << endl;
+  for (int i = 0; i < is_sort_parts2.size(); i++) {
+    cout << is_sort_parts2[i] << " ";
+  }
+  cout << endl;
+}
 
-    ths[0] =
-        thread(merge, ref(arr), steps[0].first, steps[2].first, steps[1].second,
-               steps[3].second, steps[1].second - steps[0].first + 1,
-               steps[3].second - steps[2].first + 1, 0);
-    ths[1] =
-        thread(merge, ref(arr), steps[4].first, steps[6].first, steps[5].second,
-               steps[7].second, steps[5].second - steps[4].first + 1,
-               steps[7].second - steps[6].first + 1, 1);
-    ths[0].join();
-    ths[1].join();
-
-    ths[0] =
-        thread(merge, ref(arr), steps[0].first, steps[4].first, steps[3].second,
-               steps[7].second, steps[3].second - steps[0].first + 1,
-               steps[7].second - steps[4].first + 1, 0);
-    ths[0].join();
+void change_to_zeros() {
+  for (int i = 0; i < is_sort_parts.size(); i++) {
+    is_sort_parts[i] = false;
+  }
+  for (int i = 0; i < is_sort_parts1.size(); i++) {
+    is_sort_parts1[i] = false;
+  }
+  for (int i = 0; i < is_sort_parts2.size(); i++) {
+    is_sort_parts2[i] = false;
   }
 }
 
 int main() {
-  int n;
-  cin >> n;
-    int ARRAY_SIZE = 1000000;
-  for (int i = 0; i < 1; i++) {
-      cout << "AMOUNT OF NUMBERS: " << ARRAY_SIZE << endl;
+  int ARRAY_SIZE = 1000;
+  ofstream out;
+  out.open("out.txt", ios_base::app);
+  out << "THREAD COUNT: " << THREAD_COUT << endl;
+  out << "Elements amount:\t"
+      << "TIME PARALLEL:\t"
+      << "IS SORTED:\t"
+      << "SINGLE SORT TIME:\t"
+      << "IS SORTED:" << endl;
+  for (int i = 0; i < 5; i++) {
+    // show_parts();
+    int n;
+    n = THREAD_COUT;
     vector<int> arr(ARRAY_SIZE);
     vector<int> arr_1(ARRAY_SIZE);
     generate_arr(arr);
     for (int i = 0; i < ARRAY_SIZE; i++) {
       arr_1[i] = arr[i];
     }
-    /*cout << "Before: " << endl;
-    for (auto& elem : arr) {
-      cout << elem << " ";
+
+    int step = ARRAY_SIZE / n;
+
+    vector<pair<int, int>> steps;
+    for (int i = 0; i < n; i++) {
+      steps.push_back({i * step, (i + 1) * step - 1});
     }
-    cout << endl;*/
+    steps[n - 1].second = ARRAY_SIZE - 1;
+    thread* ths = new thread[n];
+
     clock_t start, finish;
+    
+    /*-------------PARALLEL SORT-------------*/
 
-    // PARALLEL SORT
     start = clock();
-    parallel_sort(arr, n, ARRAY_SIZE);
-    finish = clock();
-    cout << i << " - " << "TIME PARALLEL: " << (float)(finish - start) / CLOCKS_PER_SEC
-         << endl;
-    cout << i << " - " << "IS SORTED: " << is_sorted(arr.begin(), arr.end()) << endl;
 
-    // SORT
+    for (int i = 0; i < n; i++) {
+      ths[i] = thread(parallel_function, ref(arr), ref(steps), i, n);
+    }
+    for (int i = 0; i < n; i++) {
+      ths[i].join();
+    }
+    finish = clock();
+    out << ARRAY_SIZE << "\t\t\t\t";
+    out << (float)(finish - start) / CLOCKS_PER_SEC << "\t\t\t";
+    out << is_sorted(arr.begin(), arr.end()) << "\t\t\t";
+    
+    /*-------------SINGLE SORT-------------*/
+
     start = clock();
     quickSort(arr_1, 0, ARRAY_SIZE - 1);
     finish = clock();
-    cout << i << " - "
-         << "TIME: " << (float)(finish - start) / CLOCKS_PER_SEC << endl;
-    cout << i << " - "
-         << "IS SORTED: " << is_sorted(arr_1.begin(), arr_1.end()) << endl;
+    out << (float)(finish - start) / CLOCKS_PER_SEC << "\t\t\t\t"; 
+    out << is_sorted(arr_1.begin(), arr_1.end()) << "\t\t";
     ARRAY_SIZE *= 10;
-    cout << endl;
-    cout << endl;
+    change_to_zeros();
+    out << endl;
   }
-
-  /*int iter = n / 2;
-  int iter_n = 2;
-  int val = 2;
-  mutex mx;
-  while (iter >= 1) {
-    for (int i = 0, j = 0; i < iter; i++, j += 2) {
-      ths[i] = thread(merge, ref(arr), steps[j].first, steps[j + 1].first,
-                      steps[j].second, steps[j + 1].second,
-                      steps[j].second - steps[j].first + 1,
-                      steps[j + 1].second - steps[j + 1].first + 1, i);
-    }
-    for (int i = 0; i < iter; i++) {
-      ths[i].join();
-    }
-    val *= 2;
-    iter /= 2;
-  }
-  */
-
-
-  /*cout << "Merge: " << endl;
-
-  for (auto& elem : arr) {
-    cout << elem << " ";
-  }
-  cout << endl;*/
-
-  //cout << "IS SORTED: " << is_sorted(arr.begin(), arr.end()) << endl;
+  out << endl;
   return 0;
 }
